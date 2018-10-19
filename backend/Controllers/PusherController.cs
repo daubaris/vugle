382cea88db88d5c35295a7dffea02629214a84a7
MantasZ <zilaitismantas@gmail.com>
2018-10-19 22:22:50 +0300

    prasau praleisti

---
+++ backend/Controllers/PusherController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VugleBE.Context;
+using VugleBE.Context.Models;
+using VugleBE.ViewModels;
+using PusherServer;
+using System.Net;
+
+namespace VugleBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PusherController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> PushNotification([FromBody] NotificationViewModel input)
+        {
+            var options = new PusherOptions
+            {
+                Cluster = "eu",
+                Encrypted = true
+            };
+
+            var pusher = new Pusher(
+              "626201",
+              "9336e0704072cd38e1db",
+              "8d7468d1ea158dc400b1",
+              options);
+
+            var result = await pusher.TriggerAsync(
+              "vugle-notifications",
+              "new-notification",
+              input);
+
+            return BadRequest();
+        }
+    }
+}
---
+++ backend/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VugleBE.ViewModels
+{
+    public class NotificationViewModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string type { get; set; }
+        public string url { get; set; }
+    }
+}
