ba76da050ad49cc1f9afab7e28c3a966be4b6917
Maciejauskas Deividas <maciedei@admins-MacBook-Pro.local>
2018-10-19 22:42:29 +0300

    Default response

--- frontend/src/app/components/chat/redux/actions.js
+++ frontend/src/app/components/chat/redux/actions.js
@@ -6,22 +6,40 @@ const actions = {
     addMessage: createAction(ADD_MESSAGE),
 };
 
+const addUserMessage = (suggestion) => (dispatch) => {
+    const message = {
+        type: 'user',
+        message: suggestion.title,
+    };
+
+    dispatch(actions.addMessage(message));
+
+    setTimeout(() => {
+        suggestion.responses.forEach((response) => {
+            if (!response.random) {
+                dispatch(addBotMessage({ title: response.title }));
+            } else {
+                const showMessage = Math.random() > response.random;
+                console.log(showMessage, response.random);
+                if (showMessage) {
+                    dispatch(addBotMessage({ title: response.title }));
+                }
+            }
+        });
+    }, 1000);
+};
+
+const addBotMessage = (suggestion) => (dispatch) => {
+    const message = {
+        type: 'bot',
+        id: new Date().getTime(),
+        message: suggestion.title,
+    };
+
+    dispatch(actions.addMessage(message));
+};
+
 export default {
-    addUserMessage: (suggestion) => (dispatch) => {
-        const message = {
-            type: 'user',
-            message: suggestion.title,
-        };
-
-        dispatch(actions.addMessage(message));
-    },
-    addBotMessage: (suggestion) => (dispatch) => {
-        const message = {
-            type: 'bot',
-            id: new Date().getTime(),
-            message: suggestion.title,
-        };
-
-        dispatch(actions.addMessage(message));
-    }
+    addUserMessage,
+    addBotMessage,
 };
--- frontend/src/app/components/chat/suggestion-bar/suggestion-bar.jsx
+++ frontend/src/app/components/chat/suggestion-bar/suggestion-bar.jsx
@@ -12,19 +12,48 @@ class SuggestionBar extends React.Component {
         super(props);
 
         this.state = {
-            suggestions: [{
-                id: 1,
-                title: 'Pranešk'
-            },{
-                id: 2,
-                title: 'Sužinok'
-            },{
-                id: 3,
-                title: 'Pažink'
-            },{
-                id: 4,
-                title: 'Pramogauk'
-            }]
+            suggestions: [
+                {
+                    id: 1,
+                    title: 'Noriu pranešti',
+                    responses: [
+                        {
+                            title: 'Pranešti?',
+                            random: 0.5,
+                        },
+                        {
+                            title: 'Ką norėtum pranešti?'
+                        }
+                    ]
+                },
+                {
+                    id: 2,
+                    title: 'Noriu sužinoti',
+                    responses: [
+                        {
+                            title: 'Ką norėtumet sužinoti?',
+                        },
+                    ]
+                },
+                {
+                    id: 3,
+                    title: 'Noriu pažinti',
+                    responses: [
+                        {
+                            title: 'Ką norėtumet pažinti?',
+                        },
+                    ]
+                },
+                {
+                    id: 4,
+                    title: 'Noriu pramogauti',
+                    responses: [
+                        {
+                            title: 'Kokios pramogos domina?',
+                        },
+                    ],
+                },
+            ]
         };
 
         this.onClick = this.onClick.bind(this);
