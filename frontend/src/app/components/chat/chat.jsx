b1e2405b27231ab49e679cf0d558ea0eb0e5b685
Augustinas <augustinas.grikis@gmail.com>
2018-10-20 21:17:28 +0300

    cheinges

--- frontend/src/app/components/chat/chat.jsx
+++ frontend/src/app/components/chat/chat.jsx
@@ -52,7 +52,7 @@ class Chat extends React.Component {
                         description: 'test description',
                         date: 'test date',
                         photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1200px-Google_Images_2015_logo.svg.png',
-                        url: 'www.google.com'
+                        url: 'https://www.google.com'
                     });
                 }, 500);
             }, 300)
