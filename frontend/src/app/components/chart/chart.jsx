5b28f1c06eaff13a7d85dda9754c2e000eba17e7
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2018-10-20 22:43:55 +0300

    fix: Display chart labels - correctly map BE model to FE model

--- frontend/src/app/components/chart/chart.jsx
+++ frontend/src/app/components/chart/chart.jsx
@@ -1,78 +1,58 @@
 import React from 'react';
 
 class Chart extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
-    }
+  parseOptions() {
+    return this.props.data.map(option => (
+      {
+        "value": option.value,
+        "label": option.title,
+      }
+    ))
+  }
 
-    render() {
-      var PieChart = require("react-chartjs").Pie;
-  
-      var chartData = [
-        {
-          value: 300,
-          color: "#00072D",
-          highlight: "#2E3453",
-        },
-        {
-          value: 50,
-          color: "#FFFFFF",
-          highlight: "#FFFFFF",
-        },
-        {
-          value: 100,
-          color: "#1098F7",
-          highlight: "#3BAAF8",
-        },
-        {
-          value: 100,
-          color: "#EBBAB9",
-          highlight: "#EEC6C5",
-        },
-        {
-          value: 100,
-          color: "#D7263D",
-          highlight: "#DE4D60",
-        },
-      ];
-  
-      var chartOptions = {
-        //Boolean - Whether we should show a stroke on each segment
-        segmentShowStroke : true,
-      
-        //String - The colour of each segment stroke
-        segmentStrokeColor : "#fff",
-      
-        //Number - The width of each segment stroke
-        segmentStrokeWidth : 2,
-      
-        //Number - The percentage of the chart that we cut out of the middle
-        percentageInnerCutout : 50, // This is 0 for Pie charts
-      
-        //Number - Amount of animation steps
-        animationSteps : 100,
-      
-        //String - Animation easing effect
-        animationEasing : "easeOutBounce",
-      
-        //Boolean - Whether we animate the rotation of the Doughnut
-        animateRotate : true,
-      
-        //Boolean - Whether we animate scaling the Doughnut from the centre
-        animateScale : false,
-        // {% raw %}
-        // //String - A legend template
-        // legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>"
-        // {% endraw %}
-      };
-      
-      return (
-          <React.Fragment>
-          <PieChart data={this.props.data} options={chartOptions}/>
-              </React.Fragment>
-      );
-    }
+  render() {
+    var PieChart = require("react-chartjs").Pie;
+
+    var chartOptions = {
+      //Boolean - Whether we should show a stroke on each segment
+      segmentShowStroke: true,
+
+      //String - The colour of each segment stroke
+      segmentStrokeColor: "#fff",
+
+      //Number - The width of each segment stroke
+      segmentStrokeWidth: 2,
+
+      //Number - The percentage of the chart that we cut out of the middle
+      percentageInnerCutout: 50, // This is 0 for Pie charts
+
+      //Number - Amount of animation steps
+      animationSteps: 100,
+
+      //String - Animation easing effect
+      animationEasing: "easeOutBounce",
+
+      //Boolean - Whether we animate the rotation of the Doughnut
+      animateRotate: true,
+
+      //Boolean - Whether we animate scaling the Doughnut from the centre
+      animateScale: false,
+      // {% raw %}
+      // //String - A legend template
+      // legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>"
+      // {% endraw %}
+    };
+
+    return (
+      <React.Fragment>
+        <PieChart data={this.parseOptions()} options={chartOptions} />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Chart;
