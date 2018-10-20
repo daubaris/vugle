48847f9297b5250686b4c36226f4a690b1d9c777
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2018-10-20 23:11:39 +0300

    feat: Add PollResult component

---
+++ frontend/src/app/pages/private/poll/poll-result.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Chart from "app/components/chart/chart";
+
+class PollResult extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    var PieChart = require("react-chartjs").Pie;
+
+    var chartOptions = {
+      //Boolean - Whether we should show a stroke on each segment
+      segmentShowStroke: true,
+
+      //String - The colour of each segment stroke
+      segmentStrokeColor: "#fff",
+
+      //Number - The width of each segment stroke
+      segmentStrokeWidth: 2,
+
+      //Number - The percentage of the chart that we cut out of the middle
+      percentageInnerCutout: 50, // This is 0 for Pie charts
+
+      //Number - Amount of animation steps
+      animationSteps: 100,
+
+      //String - Animation easing effect
+      animationEasing: "easeOutBounce",
+
+      //Boolean - Whether we animate the rotation of the Doughnut
+      animateRotate: true,
+
+      //Boolean - Whether we animate scaling the Doughnut from the centre
+      animateScale: false,
+      // {% raw %}
+      // //String - A legend template
+      // legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>"
+      // {% endraw %}
+    };
+
+    return (
+      <div>
+        <Chart data={this.props.data.options} />
+        <div>{this.props.data.title}</div>
+        <div>{this.props.data.description}</div>
+        <div>{this.props.data.date}</div></div>
+    );
+  }
+}
+
+export default PollResult;
