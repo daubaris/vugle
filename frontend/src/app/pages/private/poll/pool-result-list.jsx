4f4c1603cec379d1e4c9b93bac64657afc36b670
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2018-10-21 03:33:00 +0300

    feat: Add PollResultList page to display multiple polls

---
+++ frontend/src/app/pages/private/poll/pool-result-list.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PollResult from './poll-result';
+import { Container, Row, Col, Card, CardBody } from 'reactstrap';
+
+class PollResultList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Container>
+                {
+                    this.props.data.map(poll => (
+                        <Row>
+                            <Col lg={{ size: 12 }}>
+                                <Card>
+                                    <CardBody>
+                                        <PollResult data={poll} />
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </Row>
+                    ))
+                }
+            </Container>
+        );
+    }
+}
+
+export default PollResultList;
